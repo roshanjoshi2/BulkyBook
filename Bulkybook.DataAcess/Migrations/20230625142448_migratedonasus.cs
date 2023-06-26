bb0f673b8ed976221246d9a6ec13aa009fc7da21
roshan joshi <roshanjoshi224@gmail.com>
2023-06-26 13:22:40 +0545

    home page changes done. upcoming authentication & authorization

---
+++ Bulkybook.DataAcess/Migrations/20230625142448_migratedonasus.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulkybook.DataAcess.Migrations
+{
+    public partial class migratedonasus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
